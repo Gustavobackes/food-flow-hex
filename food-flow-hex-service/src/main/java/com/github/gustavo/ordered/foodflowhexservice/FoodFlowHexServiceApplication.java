0060ffcca14e017ce6cc588d7be4107fe983f7d4
gustavobackes <gustavo.kitzberger@projuris.com.br>
2025-02-24 21:57:49 -0300

    first commit

---
+++ food-flow-hex-service/src/main/java/com/github/gustavo/ordered/foodflowhexservice/FoodFlowHexServiceApplication.java
@@ -0,0 +1,13 @@
+package com.github.gustavo.ordered.foodflowhexservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodFlowHexServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FoodFlowHexServiceApplication.class, args);
+    }
+
+}
